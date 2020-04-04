b71e5f337822d0afb646bbb034a6c5ead153b954
GHarut <harut.gabrielian@gmail.com>
2020-04-05 03:59:20 +0400

    fix

---
+++ cache/ed/edbc011760d4e3185ce0c260faeb613b79caa2b9ad1a437d02a7b80ee9d96eec.php
@@ -0,0 +1,54 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* layout.twig */
+class __TwigTemplate_6ba3895dbbb3feae0031f7c6b4622c033beafd41d2c89c8f3a80929ae22204a8 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "asd";
+    }
+
+    public function getTemplateName()
+    {
+        return "layout.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "layout.twig", "D:\\WinNMP\\WWW\\test\\app\\View\\layout.twig");
+    }
+}
